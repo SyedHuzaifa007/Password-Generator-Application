90e571a6d61b11fe6a210ef8a7fce8b288c6fa7e
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-08 22:29:35 -0800

    Updated the app file for GUI.

--- Password Generator Application.py
+++ Password Generator Application.py
@@ -1,18 +1,20 @@
 # Importing Modules
 
 from tkinter import *
+from tkinter.messagebox import *
 import random
+import datetime
 
 # Creating Window
 
-# root = Tk()
-# root.geometry('300x300')
-# root.title('Password Generator Application')
-# Label(root,text='Welcome to Password Generator Application', font='arial 10 bold').pack()
-# Label(root,text='=========================================', font='arial 10 bold').pack()
-# Label(root,text='You can create random secure passwords here', font='arial 9 bold').pack()
-# Label(root,text='---------------------------------------------', font='arial 9 bold').pack()
-# Label(root,text='Click the below button to generate a random\npassword of 8 digits', font='arial 8 bold').pack()
+root = Tk()
+root.geometry('300x300')
+root.title('Password Generator Application')
+Label(root,text='Welcome to Password Generator Application', font='arial 10 bold').pack()
+Label(root,text='=========================================', font='arial 10 bold').pack()
+Label(root,text='You can create random secure passwords here', font='arial 9 bold').pack()
+Label(root,text='---------------------------------------------', font='arial 9 bold').pack()
+Label(root,text='Click the below button to generate a random\npassword of 8 digits', font='arial 8 bold').pack()
 
 # Making Function
 def generate_pswd():
@@ -25,21 +27,37 @@ def generate_pswd():
     SecondSymbol = str(chr(random.randint(32,64)))
     FirstNumber = str(random.randint(0,9))
     SecondNmber = str(random.randint(0,9))
+
 # Joining Characters
 
     password = str(FirstUpperCaseLetter + FirstSymbol + FirstNumber + SecondUpperCaseLetter + SecondSymbol + SecondNmber + FirstLowerCaseLetter + SecondLowerCaseLetter)
-
+    # password_show = Entry(root)
 # Shuffling the password for security
 
     def shuffle(string):
         list1 = list(password)
         shuffeld = random.shuffle(list1)
         password = str(shuffeld)
-
-# Final Password
-
     print(password)
 
-generate_pswd()
+# Saving the passwords in a file
 
-# root.mainloop()
+    with open("password.txt","a") as file:
+        date_time = str(datetime.datetime.today()) + str(datetime.datetime.now().strftime("%I%M%p"))
+        file.write(f"\n{date_time}")
+        file.write("\n--------")
+        file.write("\nPASSWORD")
+        file.write("\n========")
+        file.write(f"\n{password}")
+        file.write("\n========")
+        file.close()
+
+# def show_password():
+#     Entry.insert(0,password)
+
+# Making Button
+Button(root,text='Generate Password',font='arial 7 bold',command=generate_pswd,bg='grey').place(x=110,y=180)
+# Button(root,text='Show Generated Password',font='arial 7 bold',command=show_password,bg ='grey').place(x=110,y=200)
+# Showing the password in the Window
+
+root.mainloop()
