0680d5fcea6dd8d1d15e820fbd49dce3029d375a
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-09 18:01:50 -0800

    updated GUI file

--- Password Generator Application.py
+++ Password Generator Application.py
@@ -10,15 +10,23 @@ import datetime
 root = Tk()
 root.geometry('300x300')
 root.title('Password Generator Application')
+
+# Making Labels 
+
 Label(root,text='Welcome to Password Generator Application', font='arial 10 bold').pack()
 Label(root,text='=========================================', font='arial 10 bold').pack()
 Label(root,text='You can create random secure passwords here', font='arial 9 bold').pack()
 Label(root,text='---------------------------------------------', font='arial 9 bold').pack()
 Label(root,text='Click the below button to generate a random\npassword of 8 digits', font='arial 8 bold').pack()
+Label(root,text='This application is made by Syed Huzaifa',font='arial 7 bold').place(x=50,y=220)
+Label(root,text='Look in the terminal or in the text file created for the password',font='arial 7 bold').place(x=5,y=200)
 
 # Making Function
+
 def generate_pswd():
+
 # Making Characters
+
     FirstUpperCaseLetter = str(chr(random.randint(65,90)))
     SecondUpperCaseLetter = str(chr(random.randint(65,90)))
     FirstLowerCaseLetter = str(chr(random.randint(97,122))) 
@@ -31,7 +39,7 @@ def generate_pswd():
 # Joining Characters
 
     password = str(FirstUpperCaseLetter + FirstSymbol + FirstNumber + SecondUpperCaseLetter + SecondSymbol + SecondNmber + FirstLowerCaseLetter + SecondLowerCaseLetter)
-    # password_show = Entry(root)
+
 # Shuffling the password for security
 
     def shuffle(string):
@@ -42,7 +50,7 @@ def generate_pswd():
 
 # Saving the passwords in a file
 
-    with open("password.txt","a") as file:
+    with open("passwords.txt","a") as file:
         date_time = str(datetime.datetime.today()) + str(datetime.datetime.now().strftime("%I%M%p"))
         file.write(f"\n{date_time}")
         file.write("\n--------")
@@ -51,13 +59,11 @@ def generate_pswd():
         file.write(f"\n{password}")
         file.write("\n========")
         file.close()
-
-# def show_password():
-#     Entry.insert(0,password)
-
+    
 # Making Button
-Button(root,text='Generate Password',font='arial 7 bold',command=generate_pswd,bg='grey').place(x=110,y=180)
-# Button(root,text='Show Generated Password',font='arial 7 bold',command=show_password,bg ='grey').place(x=110,y=200)
-# Showing the password in the Window
+
+Button(root,text='Generate Password',font='arial 7 bold',command=generate_pswd,bg='grey').place(x=100,y=150)
+
+# Displaying Window
 
 root.mainloop()
